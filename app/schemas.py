b1382e0fbfbb7088ae768e926906b37ba215c270
Aluno <aluno@uninter.com>
2026-06-23 19:34:33 -0300

    Entrega Final: Back-End Raizes do Nordeste

---
+++ app/schemas.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
+from .models import PerfilEnum, CanalPedidoEnum, StatusPedidoEnum
+
+class UsuarioBase(BaseModel):
+    nome: str
+    email: EmailStr
+
+class UsuarioCreate(UsuarioBase):
+    senha: str
+    perfil: Optional[PerfilEnum] = PerfilEnum.CLIENTE
+
+class UsuarioResponse(UsuarioBase):
+    id: int
+    perfil: PerfilEnum
+    class Config:
+        from_attributes = True
+
+class LoginData(BaseModel):
+    email: EmailStr
+    senha: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class ProdutoBase(BaseModel):
+    nome: str
+    descricao: str
+    preco: float
+
+class ProdutoCreate(ProdutoBase):
+    pass
+
+class ProdutoResponse(ProdutoBase):
+    id: int
+    ativo: int
+    class Config:
+        from_attributes = True
+
+class ItemPedidoCreate(BaseModel):
+    produto_id: int
+    quantidade: int
+
+class ItemPedidoResponse(BaseModel):
+    id: int
+    produto_id: int
+    quantidade: int
+    preco_unitario: float
+    class Config:
+        from_attributes = True
+
+class PedidoCreate(BaseModel):
+    canalPedido: CanalPedidoEnum
+    itens: List[ItemPedidoCreate]
+
+class PedidoResponse(BaseModel):
+    id: int
+    usuario_id: int
+    canal_pedido: CanalPedidoEnum
+    status: StatusPedidoEnum
+    total: float
+    itens: List[ItemPedidoResponse]
+    class Config:
+        from_attributes = True
+
+class AtualizaStatusPedido(BaseModel):
+    forma_pagamento: str
